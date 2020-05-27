8c10d6951a266ce50ae7d97a2ed144c32625e92f
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2020-05-27 16:44:21 +0200

    Debug, ADC, PWM

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-* There's something called atpacks. (Here they discuss it)[https://www.avrfreaks.net/forum/solved-compiling-attiny1607-or-other-0-series1-series-avr-gcc].
-  * Better use (the official compiler)[https://www.microchip.com/mplab/avr-support/avr-and-arm-toolchains-c-compilers].
+So this is a firmware for the transmitter part of the project. Earlier version of the firmware used two multivibrators, but it prooved to be less flexible (and even more expensive) than a µC.
+
+# How to build
+Select attiny402 kit in vscode. It uses the official compiler from microchip (ancient version of GCC 5.3.0 or something) and something called an atpack.
+
+* [Here they discuss how to build with avr-gcc. CRUCIAL TO READ](https://www.avrfreaks.net/forum/solved-compiling-attiny1607-or-other-0-series1-series-avr-gcc).
+  * Better use [the official compiler](https://www.microchip.com/mplab/avr-support/avr-and-arm-toolchains-c-compilers).
+
+# How to flash
+* Grab [this](https://github.com/mraardvark/pyupdi).
+* `sudo python ./setup.py install`
+* Connect everything as described in the README.md from pyupdi.
+
+``` sh
+pyupdi.py -d tiny402 -c /dev/ttyUSB0 -f build/transmitter.hex
+```
--- src/main.cc
+++ src/main.cc
@@ -1,52 +1,139 @@
-/*
- * GccApplication1.cpp
- *
- * Created: 2019-05-25 10:36:15
- * Author : Dell Latitude E6540
- */
+/****************************************************************************
+ *                                                                          *
+ *  Author : lukasz.iwaszkiewicz@gmail.com                                  *
+ *  ~~~~~~~~                                                                *
+ *  License : see COPYING file for details.                                 *
+ *  ~~~~~~~~~                                                               *
+ ****************************************************************************/
 
-#include <avr/io.h>
-#define F_CPU 4000000
 #include <avr/interrupt.h>
+#include <avr/io.h>
+#include <stdint.h>
+#include <string.h>
 #include <util/delay.h>
 
-#define ENCODER1_PIN 6
-#define ENCODER2_PIN 7
+enum Pin { redLed = 0, greenLed = 1, senseOn = 2, ir = 6, batSense = 7 };
 
-#define SPI_MOSI_PIN 1
-#define SPI_MISO_PIN 2
-#define SPI_SCK_PIN 3
-#define SPI_CS_PIN 0
+static constexpr long int CLK_PER_Hz = 3300000;
+static constexpr long int CARRIER_Hz = 56000;
+static constexpr uint8_t PWM_CNT_VALUE = uint8_t ((CLK_PER_Hz / CARRIER_Hz) + 0.5F);
 
-int main (void)
+void reverse (char s[])
 {
+        int i, j;
+        char c;
+
+        for (i = 0, j = strlen (s) - 1; i < j; i++, j--) {
+                c = s[i];
+                s[i] = s[j];
+                s[j] = c;
+        }
+}
+
+void itoa (unsigned int n, char s[])
+{
+        int i;
+
+        i = 0;
+
+        do {                           /* generate digits in reverse order */
+                s[i++] = n % 10 + '0'; /* get next digit */
+        } while ((n /= 10) > 0);       /* delete it */
+
+        s[i] = '\0';
+
+        reverse (s);
+}
+
+void print (uint8_t const *data, int len)
+{
+        for (int i = 0; i < len; ++i) {
+
+                // Wait until writing to the register is allowed
+                while ((USART0.STATUS & USART_DREIF_bm) == 0) {
+                }
+
+                USART0.TXDATAL = *(data + i);
+
+                // Wait until sent. Not strictly necessary I think.
+                while ((USART0.STATUS & USART_TXCIF_bm) == 0) {
+                }
+        }
+}
+
+void print (const char *str) { print (reinterpret_cast<const uint8_t *> (str), strlen (str)); }
+
+void print (unsigned int i)
+{
+        char buf[11];
+        itoa (i, buf);
+        print (buf);
+}
+
+/**
+ * CPU is running @ 20MHz, so CLK_PER is 3.3MHz
+ */
+int main ()
+{
+        // All pins as inputs initially.
         PORTA.DIR = 0;
 
-        // Init encoder inputs
-        PORTA.DIR |= (1 << ENCODER1_PIN | 1 << ENCODER2_PIN);
+        // IR as output
+        PORTA.DIR |= (1 << Pin::ir);
 
-        // Init SPI
-        PORTA.DIR |= (1 << SPI_MOSI_PIN | 1 << SPI_SCK_PIN | 1 << SPI_CS_PIN);
+        /*--------------------------------------------------------------------------*/
+        // UART for debugging.
+#ifdef WITH_DEBUG
+        volatile uint8_t tmp = PORTA.OUT;
+        tmp |= (1 << Pin::ir);
+        PORTA.OUT = tmp;               // High
+        USART0.BAUD = 115;             // 115200
+        USART0.CTRLB |= USART_TXEN_bm; // Enable transmitter
+#endif
+        /*--------------------------------------------------------------------------*/
+        // PWM for the IR.
 
-        //        // Enable SPI, Set as Master
-        //        // Prescaler: Fosc/16, Enable Interrupts
-        //        //The MOSI, SCK pins are as per ATMega8
-        //        SPCR=(1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPIE);
+        // // Load CCMP register with the period and duty cycle of the PWM
+        // TCB0.CCMPL = PWM_CNT_VALUE;
+        // TCB0.CCMPH = PWM_CNT_VALUE / 2;
+        // TCB0.CNT = 0;
 
-        //        SPI0.CTRLA
+        // // Enable TCB clock == CLK_PER
+        // TCB0.CTRLA |= TCB_ENABLE_bm;
 
-        //        // Enable Global Interrupts
-        //        sei();
+        // // Enable Pin Output and configure TCB in 8-bit PWM mode
+        // TCB0.CTRLB |= TCB_CCMPEN_bm;
+        // TCB0.CTRLB |= TCB_CNTMODE_PWM8_gc;
 
-        /* Replace with your application code */
-        while (1) {
-                volatile uint8_t tmp = PORTA.OUT;
-                tmp |= 0b11000000;
-                PORTA.OUT = tmp;
-                _delay_ms (2);
+        /*--------------------------------------------------------------------------*/
 
-                tmp &= ~0b11000000;
-                PORTA.OUT = tmp;
-                _delay_ms (2);
+        ADC0.CTRLB |= (ADC_SAMPNUM0_bm | ADC_SAMPNUM1_bm);                                    // Accumulate 8 consecutive results
+        ADC0.CTRLC |= (ADC_PRESC0_bm | ADC_PRESC1_bm | ADC_PRESC2_bm) /* | ADC_REFSEL0_bm */; // Prescaler set to divide by 256 (slow)
+        ADC0.MUXPOS = 0x07;                                                                   // measure on PA7
+        // ADC0.MUXPOS = 0x00;
+        ADC0.CTRLA |= ADC_ENABLE_bm; // Enable in 10bits resolution. Use internal voltage reference
+
+        /*--------------------------------------------------------------------------*/
+
+        while (true) {
+                // volatile uint8_t tmp = PORTA.OUT;
+                // tmp |= (1 << Pin::ir);
+                // PORTA.OUT = tmp;
+                // _delay_ms (1);
+
+                // tmp &= ~(1 << Pin::ir);
+                // PORTA.OUT = tmp;
+                _delay_ms (50);
+
+                // Start the ADC conversion
+                ADC0.COMMAND = ADC_STCONV_bm; // This starts the measurement
+
+                // Wait for the converison to finish
+                while ((ADC0.COMMAND & ADC_STCONV_bm) != 0 && (ADC0.INTFLAGS & ADC_RESRDY_bm) == 0) {
+                }
+
+                int adcResult = ADC0.RES;
+                print (adcResult);
+                print ("\r\n");
         }
 }
